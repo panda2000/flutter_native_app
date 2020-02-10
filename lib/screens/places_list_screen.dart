801164788b3388bdaf891235b96209a5d0e848d7
pandaprg <pandaprg@gmail.com>
2020-02-10 22:44:49 +0400

    Initial commit

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PlaceListScreen extends StatelessWidget {
+
+  static const String routeName = "/place_list_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
